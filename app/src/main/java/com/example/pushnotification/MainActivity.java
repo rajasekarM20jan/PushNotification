3554f04134ce3a43afb043ebc36be3328f136c7c
Rajasekar <rs20jn1998s@gmail.com>
2023-04-29 17:35:37 +0530

    init

--- app/src/main/java/com/example/pushnotification/MainActivity.java
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -23,14 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         subscribeButton=findViewById(R.id.subscribeButton);
         topicET=findViewById(R.id.topicET);
-
-        // Create a notification channel
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("my_channel_id", "My Channel", NotificationManager.IMPORTANCE_HIGH);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-        //test
 
         subscribeButton.setOnClickListener(l->{
             try{
--- app/src/main/java/com/example/pushnotification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/pushnotification/MyFirebaseMessagingService.java
@@ -1,12 +1,31 @@
 package com.example.pushnotification;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    Uri soundUri;
 
     private static final String TAG = "MyFirebaseMessagingService";
 
@@ -14,21 +33,54 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        // Get the notification payload data
-        String title = remoteMessage.getNotification().getTitle();
-        String message = remoteMessage.getNotification().getBody();
 
-        // Create a notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my_channel_id")
+
+        if (remoteMessage.getNotification() != null) {
+            // Get the notification payload data
+            String title = remoteMessage.getNotification().getTitle();
+            String message = remoteMessage.getNotification().getBody();
+            sendNotification(title, message);
+        }
+
+    }
+    private void sendNotification(String title, String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "12345678";
+        String packageName = getApplicationContext().getPackageName();
+        int resourceId = getResources().getIdentifier("custom_tone", "raw", packageName);
+        soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + File.pathSeparator + File.separator
+                + File.separator + getApplicationContext().getPackageName() + File.separator  + resourceId);
+
+        Notification ntf = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.notification)
                 .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true);
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL & ~NotificationCompat.DEFAULT_SOUND)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent).build();
 
-        // Show the notification
+
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(0, builder.build());
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create a default notification channel for the app
+            NotificationChannel channel = new NotificationChannel(channelId, "Channel Name", NotificationManager.IMPORTANCE_HIGH);
+            AudioAttributes attributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build();
+            channel.setSound(soundUri, attributes);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+
+
+        notificationManager.notify(0, ntf);
     }
 }
 
